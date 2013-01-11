d8f6d684aab9e674fefb9e214efdff9367588791
k.shaplyko <k.shaplyko@itransition.com>
2013-01-12 00:48:41 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LightSwords::Application.config.secret_token = '2b9c1a4a65a6e1097221ec7c0f50c231fc4708dbaffc179a08134d02c3d6c55528c7ee3caec9014ac7283cd176c4f1da4abe819fd7f4d576d5d648e10ad6da28'
